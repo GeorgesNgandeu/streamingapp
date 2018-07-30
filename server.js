c70c33d1c04008c5c3fd61942daac7fb11e38226
GeorgesNgandeu <georgesngandeu@gmail.com>
2018-07-30 18:39:26 +0200

    just some modifications

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ const express = require('express');
 
 var app = express();
 
+app.use(express.static(__dirname ));
+
 app.get('/', (req,res) => {
 	res.send('Hello Express')
+});
+
+app.listen(3000, () => {
+	console.log('server up and running on port 3000');
 });
