c958f7a850b27370f17fb9ae75d4db1335c90a6e
GeorgesNgandeu <georgesngandeu@gmail.com>
2018-07-30 18:49:40 +0200

    just do it

--- server.js
+++ server.js
@@ -2,12 +2,14 @@ const express = require('express');
 
 var app = express();
 
+const port = process.env.PORT || 3000;
+
 app.use(express.static(__dirname ));
 
 app.get('/', (req,res) => {
 	res.send('Hello Express')
 });
 
-app.listen(3000, () => {
-	console.log('server up and running on port 3000');
+app.listen(port, () => {
+	console.log(`server up and running on port ${port}`);
 });
